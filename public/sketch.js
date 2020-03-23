48ecd3a11876e4e4c9fd23078e8270d0a03fadd3
Schuyler deVos <schuylerdevos@gmail.com>
2020-03-23 18:46:47 -0400

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -2,8 +2,18 @@
 // Open and connect input socket
 let socket = io();
 
+/*
+to-do:
+make sure it works??
+*/
+
+// Listen for confirmation of connection
+socket.on('connect', function() {
+  console.log("Connected", socket.id);
+});
+
 //keep track of all users
-let users = {};
+let players;
 // Is it my turn?
 let myTurn = false;
 //keep track of color
@@ -11,53 +21,68 @@ let myColor;
 //keep track of ink;
 let ink = 0;
 
-// Listen for confirmation of connection
-socket.on('connect', function() {
-  console.log("Connected", socket.id);
+let getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-  socket.on('setPrompt', function ({ prompt }) {
-    // TODO fill this in
-    // Update prompt on screen and erase canvas to make way for new drawing
-  })
+socket.on('setPrompt', function ({ prompt }) {
+  console.log(prompt)
+});
 
-  // Listen for my turn
-  socket.on('currentPlayer', function(currentPlayer) {
-    if(currentPlayer === socket.id){
-      myTurn = true;
-      ink = 255;
-    } else {
-      console.log(`Current player: ${currentPlayer}`)
-      myTurn = false;
-    }
-  });
+socket.on('gameFinished', function(){
+  background(255);
+  text("DONE", width/2, height/2);
+});
 
-  // Listen for changes to text
-  socket.on('drawPoint', function(drawData) {
+socket.on('currentPlayer', function({ currentPlayer }){
+  if(currentPlayer === socket.id){
+    console.log(`ITS YOUR TURN DAWG`)
+    myTurn = true;
+    ink = 255;
+  } else {
+    console.log(`Current player: ${currentPlayer}`)
+  }
+});
 
-    // Update string on screen
-    drawLine(drawData);
-  });
+// Listen for changes to text
+socket.on('drawPoint', function(drawData) {
+  // Update line on screen
+  drawLine(drawData);
+});
+
+socket.on('allPlayers', function(data) {
+  players = data;
 });
 
 function setup() {
 
+  myColor = getRandomColor();
+
   frameRate(30);
   createCanvas(windowWidth, windowHeight);
+
 }
 
 // Draw line
 function drawLine(drawData) {
   //then we are drawing the line in the correct color
-  stroke(drawData.color,drawData.inkLeft);
-  line(drawData.x,drawData.y,drawData.pX,drawData.pY);
+  stroke(drawData.data.color,drawData.data.inkLeft);
+  line(drawData.data.x,drawData.data.y,drawData.data.pX,drawData.data.pY);
 }
 
-//mouseDragged function
-//so IF IT'S YOUR TURN AND YOU'VE DRAWN, WE NEED TO EMIT THIS DATA SO EVERYONE ELSE
-//CAN DRAW IT TOO, RIGHT?
-//wait, we just want this to send the coordinates right. we dont actually want it drawn here,
-//we want it drawn through drawline
-//yes, that's what the draw in server does, ok cool
+function keyPressed() {
+  if (keyCode === ENTER) {
+    if (myTurn) {
+      socket.emit('finishRound');
+    };
+  }
+}
+
 function mouseDragged() {
   // Ignore if it's not your turn
   if (!myTurn) {
@@ -68,58 +93,23 @@ function mouseDragged() {
     let y = mouseY / height;
     let pX = pmouseX / width;
     let pY = pmouseY / height;
-    socket.emit('draw', {
+    socket.emit('drawPoint', {
       x: x,
       y: y,
       pX: pX,
       pY: pY,
       inkLeft: ink,
-      color: '#111'
+      color: myColor
     });
     if (ink < 1) {
       myTurn = false;
-      socket.emit('next');
+      socket.emit('nextPlayer');
     }
     ink -= 1.5;
   }
 }
 
-//TITOS STUFF
+//adityas STUFF i dont in there yet
 /*
-let socket = io()
 
-// Listen for confirmation of connection
-socket.on('connect', function() {
-  console.log("Connected")
-})
-
-socket.on('setPrompt', function ({ prompt }) {
-  console.log(prompt)
-})
-
-socket.on('currentPlayer', function({ currentPlayer }){
-  if(currentPlayer === socket.id){
-    console.log(`ITS YOUR TURN DAWG`)
-  } else {
-    console.log(`Current player: ${currentPlayer}`)
-  }
-})
-
-socket.on('allPlayers', function({ players }) {
-  console.log(players)
-})
-
-socket.on('gameFinished', function(){
-  console.log('game has finished dawg, go home')
-})
-
-function finishRound(){
-  socket.emit('finishRound')
-}
-
-function setup(){
-  createCanvas(800, 600);
-  background(0);
-} 
 */
-
